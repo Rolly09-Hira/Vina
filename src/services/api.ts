aa48e12129dceb695dce68509fec4b421bf70374
Rolly09-Hira <roilyandriamahery911@gmail.com>
2026-03-30 14:14:16 +0300

    newapi

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://because-thinking-mhz-gather.trycloudflare.com/api',  // URL de render
+  baseURL: 'https://database-deutschland-venture-believes.trycloudflare.com/api',  // URL de render
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
