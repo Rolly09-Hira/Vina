58141427b20507407fc71a2eb4220303500adb54
Rolly09-Hira <roilyandriamahery911@gmail.com>
2026-04-19 14:56:30 +0300

    apres panne

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://says-send-kernel-diy.trycloudflare.com/api',  // URL de render
+  baseURL: 'https://backvina-1.onrender.com/api',  // URL de render
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
